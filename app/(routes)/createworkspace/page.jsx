b3ea36e4e044d51c3955217570a2fe8f8f62ebb8
Ayushi <ayushinarang21@gmail.com>
2024-10-07 05:20:32 +0530

    firebase config and setup

--- app/(routes)/createworkspace/page.jsx
+++ app/(routes)/createworkspace/page.jsx
@@ -3,7 +3,9 @@ import CoverPicker from '@/app/_components/CoverPicker';
 import EmojiPickerComponent from '@/app/_components/EmojiPickerComponent';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import { db } from '@/config/firebaseConfig';
 import { useAuth, useUser } from '@clerk/nextjs';
+import { doc, setDoc } from 'firebase/firestore';
 import { Loader2Icon, SmilePlus } from 'lucide-react';
 import Image from 'next/image'
 import { useRouter } from 'next/navigation';
@@ -20,12 +22,38 @@ function CreateWorkspace() {
     const [loading,setLoading]=useState(false);
     const router=useRouter();
 
+    const OnCreateWorkspace=async()=>{
+        setLoading(true);
+        const workspaceId=Date.now();
+        const result=await setDoc(doc(db,'Workspace',workspaceId.toString()),{
+            workspaceName:workspaceName,
+            emoji:emoji,
+            coverImage:coverImage,
+            createdBy:user?.primaryEmailAddress?.emailAddress,
+            id:workspaceId,
+            orgId:orgId?orgId:user?.primaryEmailAddress?.emailAddress
+        });
 
-    const OnCreateWorkspace=()=>{
-        console.log("workspace created!")
+        const docId=uuid4();
+        await setDoc(doc(db,'workspaceDocuments',docId.toString()),{
+            workspaceId:workspaceId,
+            createdBy:user?.primaryEmailAddress?.emailAddress,
+            coverImage:null,
+            emoji:null,
+            id:docId,
+            documentName:'Untitled Document',
+            documentOutput:[]
+        })
+
+        await setDoc(doc(db,'documentOutput',docId.toString()),{
+            docId:docId,
+            output:[]
+        })
+
+        setLoading(false);
+        router.replace('/workspace/'+workspaceId+"/"+docId);
+        
     }
-
- 
   return (
     <div className='p-10 md:px-36 lg:px-64 xl:px-96 py-28'>
         <div className='shadow-2xl rounded-xl'>
---
+++ config/firebaseConfig.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore} from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "notebox-4331f.firebaseapp.com",
+  projectId: "notebox-4331f",
+  storageBucket: "notebox-4331f.appspot.com",
+  messagingSenderId: "643791293098",
+  appId: "1:643791293098:web:fa64efb24b48e12c5fded3",
+  measurementId: "G-C93D5VHL9M"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db=getFirestore(app)
